af3a0118d9308d027ed5119b51c61f213c1e823a
Aku Silvennoinen <akusilvennoinen@Macintosh-31.local>
2014-03-25 22:20:11 +0200

    päivitys

---
+++ Virtuaaliviivakoodigeneraattori/src/main/java/virtuaaliviivakoodigeneraattori/virtuaaliviivakoodigeneraattori/kayttoliittyma/suoritteet/SuoritteetTaulukko.java
@@ -0,0 +1,70 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+package virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.kauttoliittyma.laskut;
+
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.kauttoliittyma.asiakkaat.*;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.DefaultTableModel;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.kauttoliittyma.TableModelSolujenMuokkaaminenEstetty;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.logiikka.Asiakas;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.logiikka.Lataaja;
+
+/**
+ *
+ * @author Augustus58
+ */
+public class LaskutTaulukko {
+
+    private final JTable taulukko;
+    private final TableModelSolujenMuokkaaminenEstetty model;
+    private final ListSelectionModel selectionModel;
+    private final Lataaja lataaja;
+
+    public LaskutTaulukko(Lataaja lataaja) {
+        this.lataaja = lataaja;
+        this.taulukko = new JTable();
+        this.model = new TableModelSolujenMuokkaaminenEstetty(new Object[][]{}, new Object[]{"Nimi", "Katuosoite", "Postinumero", "Kaupunki", "Asiakasnumero", "Laskuja lähetetty"});            
+        this.taulukko.setModel(model);
+        this.selectionModel = this.taulukko.getSelectionModel();
+        this.selectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        taulukko.setAutoCreateRowSorter(true);
+        muodostaLaskutTaulukko();
+    }
+
+    public final void muodostaLaskutTaulukko() {
+        if (!lataaja.getLadattuTietovarasto().getLaskut().isEmpty()) {
+            for (int i = 0; i < lataaja.getLadattuTietovarasto().getLaskut().size(); i++) {
+                model.addRow(lataaja.getLadattuTietovarasto().getAsiakkaat().get(i).getAsiakkaanTiedotTaulukossa());
+            }
+        }
+    }
+    
+    public void addAsiakkaatTaulukkoRivi(Asiakas asiakas) {
+        model.addRow(asiakas.getAsiakkaanTiedotTaulukossa());
+    }
+
+    public JTable getTaulukko() {
+        return taulukko;
+    }
+
+    public DefaultTableModel getModel() {
+        return model;
+    }
+
+    public ListSelectionModel getSelectionModel() {
+        return selectionModel;
+    }
+
+    public String getValueString(Integer x, Integer y) {
+        return (getModel().getValueAt(x, y).toString());
+    }
+
+    public void reset() {
+        model.setRowCount(0);
+    }
+
+}
---
+++ Virtuaaliviivakoodigeneraattori/src/main/java/virtuaaliviivakoodigeneraattori/virtuaaliviivakoodigeneraattori/kayttoliittyma/yhteenveto/muokkaa/MuokkaaLaskuttajanTietojaIkkunaMuokkaaKuuntelija.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.kauttoliittyma.yhteenveto;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.kauttoliittyma.NappulaLukko;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.logiikka.Laskuttaja;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.logiikka.Lataaja;
+import virtuaaliviivakoodigeneraattori.virtuaaliviivakoodigeneraattori.logiikka.Tilinumero;
+
+/**
+ *
+ * @author Augustus58
+ */
+public class LisaaLaskuttajanTiedotIkkunaLisaaKuuntelija implements ActionListener {
+
+    private JTextField nimiKentta;
+    private JTextField katuosoiteKentta;
+    private JTextField postinumeroKentta;
+    private JTextField kaupunkiKentta;
+    private JTextField yritykseNimiTeksti;
+    private JTextField alvTunnisteKentta;
+    private JTextField tilinumeroKentta;
+    private JTextField tilinumeronPankkiKentta;
+    private JTextField tilinumeronSwiftBicKentta;
+    private JTextField puhelinnumeroKentta;
+    private JTextField sahkopostiKentta;
+    private JTextField laskujaLahetettyKentta;
+    private final Lataaja lataaja;
+    private final JFrame frame;
+    private final NappulaLukko lukko;
+    private final LaskuttajaOsioJPanel panel;
+
+    public LisaaLaskuttajanTiedotIkkunaLisaaKuuntelija(JTextField nimiKentta, JTextField katuosoiteKentta, JTextField postinumeroKentta, JTextField kaupunkiKentta, JTextField yritykseNimiTeksti, JTextField alvTunnisteKentta, JTextField tilinumeroKentta, JTextField tilinumeronPankkiKentta, JTextField tilinumeronSwiftBicKentta, JTextField puhelinnumeroKentta, JTextField sahkopostiKentta, JTextField laskujaLahetettyKentta, Lataaja lataaja, JFrame frame, NappulaLukko lukko, LaskuttajaOsioJPanel panel) {
+        this.nimiKentta = nimiKentta;
+        this.katuosoiteKentta = katuosoiteKentta;
+        this.postinumeroKentta = postinumeroKentta;
+        this.kaupunkiKentta = kaupunkiKentta;
+        this.yritykseNimiTeksti = yritykseNimiTeksti;
+        this.alvTunnisteKentta = alvTunnisteKentta;
+        this.tilinumeroKentta = tilinumeroKentta;
+        this.tilinumeronPankkiKentta = tilinumeronPankkiKentta;
+        this.tilinumeronSwiftBicKentta = tilinumeronSwiftBicKentta;
+        this.puhelinnumeroKentta = puhelinnumeroKentta;
+        this.sahkopostiKentta = sahkopostiKentta;
+        this.laskujaLahetettyKentta = laskujaLahetettyKentta;
+        this.lataaja = lataaja;
+        this.frame = frame;
+        this.lukko = lukko;
+        this.panel = panel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Tilinumero tilinumero = new Tilinumero(tilinumeroKentta.getText(), tilinumeronPankkiKentta.getText(), tilinumeronSwiftBicKentta.getText());
+        if (!tilinumero.tarkistaTilinumero(tilinumero.getTilinumero())) {
+            throw new IllegalArgumentException("Tilinumero on virheellinen.");
+        }
+
+        Laskuttaja laskuttaja = new Laskuttaja(nimiKentta.getText(),
+                katuosoiteKentta.getText(),
+                postinumeroKentta.getText(),
+                kaupunkiKentta.getText(),
+                yritykseNimiTeksti.getText(),
+                alvTunnisteKentta.getText(),
+                tilinumero,
+                puhelinnumeroKentta.getText(),
+                sahkopostiKentta.getText(),
+                Integer.parseInt(laskujaLahetettyKentta.getText()));
+
+        if (laskuttaja.onkoTiedotOikeanlaiset()) {
+            lataaja.getLadattuTietovarasto().setLaskuttaja(laskuttaja);
+        } else {
+            throw new IllegalArgumentException("Jokin syöte on virheellinen.");
+        }
+        panel.setLaskuttajaOlemassa(true);
+        panel.paivitaSisalto();
+        suljeIkkuna();
+    }
+
+    private void suljeIkkuna() {
+        frame.dispose();
+    }
+}
